2d5c42889acff63378a72afa6c2a234ff789d919
Thomas Flaherty <tom.flaherty03@gmail.com>
2023-10-01 20:09:10 +0100

    Hopefully I sort this out tomorrow

--- bin_day_arduino/bin_day_arduino.ino
+++ bin_day_arduino/bin_day_arduino.ino
@@ -1,10 +1,31 @@
 #include <WiFiNINA.h>
+#include "time_magic_numbers.hpp"
 #include "wifi_settings.hpp"
+#include "auto_generated_data.hpp"
 
-#define DEBUG_OUTPUT
+// Arduino will get stuck if debug is enabled but the board is not connected to a pc
+// #define DEBUG_OUTPUT
 
-#define MAX_CONNECT_ATTEMPTS  ( 3 )
-#define MAX_GET_EPOCH_ATTEMPS ( 50 )
+// #define LED_TEST
+// #define BUTTON_TEST
+
+#define BUTTON_PIN                ( 20 )
+
+#define MAX_CONNECT_ATTEMPTS      ( 3 )
+#define MAX_GET_EPOCH_ATTEMPS     ( 50 )
+
+// Delay in seconds between using WiFi to check the time
+#define TIME_DOWNLOAD_PERIOD      ( SECONDS_IN_TEN_HOURS )
+// Button poll frequency in milliseconds
+#define BUTTON_POLL_FREQUENCY_MS  ( 50 )
+// LED toggle period in milliseconds. E.g. if value is 500, LED will toggle every 500 ms
+#define LED_TOGGLE_PERIOD         ( 1000 )
+
+// Global Variables
+uint64_t currentTime;               // Seconds since 1970
+uint64_t timeSinceTimeDownload = 0; // Seconds since WiFi.getTime() was last called
+uint64_t flashIntervalThing[NUMBER_OF_DATASETS];
+bool isItBinDay[NUMBER_OF_DATASETS];
 
 void setup() 
 {
@@ -15,17 +36,55 @@ void setup()
   while ( !Serial );
   #endif
 
+  // Setup output pins
   pinMode( LED_BUILTIN, OUTPUT );
+  pinMode( BUTTON_PIN, INPUT );
+  for ( int dataSetNumber = 0; dataSetNumber < NUMBER_OF_DATASETS; dataSetNumber++ )
+  {
+    pinMode( setArray[dataSetNumber].ledPin, OUTPUT );
+  }
+
+  #ifdef LED_TEST
+  // Test the LEDs
+  digitalWrite( LED_BUILTIN, HIGH );
+  delay(500);
+  digitalWrite( LED_BUILTIN, LOW );
+  for ( int dataSetNumber = 0; dataSetNumber < NUMBER_OF_DATASETS; dataSetNumber++ )
+  {
+    digitalWrite( setArray[dataSetNumber].ledPin, HIGH );
+    delay(500);
+    digitalWrite( setArray[dataSetNumber].ledPin, LOW );
+  }
+  #endif
+
+  #ifdef BUTTON_TEST
+  bool buttonState;
+  while ( true )
+  {
+    // Read the input pin
+    buttonState = digitalRead( BUTTON_PIN );
+    // Write to the built in led
+    digitalWrite( LED_BUILTIN, buttonState );
+    delay(10);
+  }
+  #endif
 }
 
 void loop() 
 {
   // Get unix time from WiFi
-  uint64_t theTime = 0;
-  theTime = getUnixFromWifi();
-  Serial.println(theTime);
+  currentTime = getUnixFromWifi();
+  Serial.println( currentTime );
 
-  delay( 1000 );
+  checkForBinDay();
+
+  /*
+  Get the time from wifi every X hours
+  Blink the built-in led if there is no wifi
+  Blink an led if its bin day, until the button is pressed
+
+
+  */
 }
 
 uint64_t getUnixFromWifi()
@@ -39,10 +98,14 @@ uint64_t getUnixFromWifi()
   // Check the WiFi module
   if ( WiFi.status() == WL_NO_MODULE )
   {
-    errorFlash();
+    digitalWrite( LED_BUILTIN, HIGH );
     #ifdef DEBUG_OUTPUT
     Serial.println( "Error: WiFi module not found" );
     #endif
+    while ( true )
+    {
+      delay(10000);
+    }
   }
 
   // Connect to WiFi
@@ -91,13 +154,7 @@ uint64_t getUnixFromWifi()
   return unixEpoch;
 }
 
-void errorFlash()
+void checkForBinDay()
 {
-  bool state = false;
-  for ( ;; )
-  {
-    digitalWrite( LED_BUILTIN, state );
-    state = !state;
-    delay( 500 );
-  }
+  //
 }
