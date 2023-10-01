592deddf0204fa598736344c5e7d0490d78be098
Thomas Flaherty <tom.flaherty03@gmail.com>
2023-10-01 10:11:53 +0100

    Functionality pretty much finished for header generator

--- header_generator.py
+++ header_generator.py
@@ -28,23 +28,24 @@ def main():
                 unix_timestamp = date_to_unix(int(date[0]), int(date[1]), int(date[2]))
                 unix_times.append(unix_timestamp)
         write_array_to_cpp(cpp_filename, data_set[0], unix_times, data_set[1])
-
+    # Definition of array of structs
+    write_struct_array(cpp_filename, [i[0] for i in SETTINGS])
 
 def init_files(cpp_filename, hpp_filename, number_of_datasets):
     with open(cpp_filename, 'w') as cpp_file: # cpp file
-        cpp_file.write("#include <stdint>\n")
+        cpp_file.write("#include <stdint.h>\n")
         cpp_file.write("#include \"" + os.path.split(hpp_filename)[-1] + "\"\n\n")
-        cpp_file.write("t_set setArray[NUMBER_OF_DATASETS]\n\n")
+        cpp_file.write("t_binDay setArray[NUMBER_OF_DATASETS]\n\n")
     with open(hpp_filename, 'w') as hpp_file: # hpp file
-        hpp_file.write("#include <stdint>\n\n")
+        hpp_file.write("#include <stdint.h>\n\n")
         hpp_file.write("#define NUMBER_OF_DATASETS ( " + str(number_of_datasets) + " )\n\n")
         hpp_file.write("typedef struct\n")
         hpp_file.write("{\n")
         hpp_file.write("\tuint64_t *arrayStart;\n")
         hpp_file.write("\tuint16_t arrayLength;\n")
         hpp_file.write("\tuint8_t ledPin;\n")
-        hpp_file.write("} t_set;\n\n")
-        hpp_file.write("extern t_set setArray[NUMBER_OF_DATASETS];\n")
+        hpp_file.write("} t_binDay;\n\n")
+        hpp_file.write("extern t_binDay setArray[NUMBER_OF_DATASETS];\n")
 
 def write_array_to_cpp(cpp_filename, csv_filename, unix_timestamps, led_pin):
     # Open the cpp file ready to append
@@ -53,10 +54,24 @@ def write_array_to_cpp(cpp_filename, csv_filename, unix_timestamps, led_pin):
         cpp_file.write("uint64_t " + generate_array_name(csv_filename) + "[] = {\n")
         for item in unix_timestamps:
             cpp_file.write("\t" + str(item) + ",\n")
-        cpp_file.write("}\n")
-        cpp_file.write("uint16_t " + generate_array_name(csv_filename) + "Length = " + str(len(unix_timestamps)) + "\n")
-        cpp_file.write("uint8_t " + generate_array_name(csv_filename) + "LedPin = " + str(led_pin) + "\n\n")
+        cpp_file.write("};\n")
+        cpp_file.write("uint16_t " + generate_array_name(csv_filename) + "Length = " + str(len(unix_timestamps)) + ";\n")
+        cpp_file.write("uint8_t " + generate_array_name(csv_filename) + "LedPin = " + str(led_pin) + ";\n\n")
 
+def write_struct_array(cpp_filename, csv_filenames):
+    with open(cpp_filename, 'a') as cpp_file:
+        cpp_file.write("// Array of structs definition\n\n")
+        cpp_file.write("t_binDay setArray[NUMBER_OF_DATASETS] = {\n")
+        for csv_filename in csv_filenames:
+            string = "\t{"
+            string += ".arrayStart = " + generate_array_name(csv_filename)
+            string += ", "
+            string += ".arrayLength = " + generate_array_name(csv_filename) + "Length"
+            string += ", "
+            string += ".ledPin = " + generate_array_name(csv_filename) + "LedPin"
+            string += "},\n"
+            cpp_file.write(string)
+        cpp_file.write("};\n")
 
 def date_to_unix(day, month, year, hour=0, minute=0, second=0) -> int:
     return int(time.mktime(datetime.datetime(year, month, day, hour, minute, second).timetuple()))
@@ -73,7 +88,7 @@ def generate_array_name(csv_filename) -> str:
 #             data.append(row)
 #     # And write some data
 #     with open(hpp_file, 'w') as header_file:
-#         header_file.write("#include <stdint>\n\n")
+#         header_file.write("#include <stdint.h>\n\n")
 #         header_file.write("uint64_t " + array_name + "[] = {\n")
 #         for date in data:
 #             unix = date_to_unix(int(date[0]), int(date[1]), int(date[2]))
